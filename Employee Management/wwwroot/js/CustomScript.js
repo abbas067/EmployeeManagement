82ad7be8a269a3114a9c4313cca9685c9d955538
abbas <abbas@ABBAS>
2021-07-16 15:37:35 +0530

    delete identity user

--- Employee Management/wwwroot/js/CustomScript.js
+++ Employee Management/wwwroot/js/CustomScript.js
@@ -1,1 +1,13 @@
-﻿
+﻿    function confirmDelete(uniqueId, isDeleteClicked) {
+        var deleteSpan = 'deleteSpan' + uniqueId;
+        var confirmDeleteSpan = 'confirmDeleteSpan' + uniqueId;
+        if (isDeleteClicked) {
+        $('#' + deleteSpan).hide();
+            $('#' + confirmDeleteSpan).show();
+        }
+        else {
+        $('#' + deleteSpan).show();
+            $('#' + confirmDeleteSpan).hide();
+
+
+        }
